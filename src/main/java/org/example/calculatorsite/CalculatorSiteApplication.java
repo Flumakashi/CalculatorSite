2093f0a993f4a57940800330732ef0fca4686668
Flumakashi <flumakashi@gmail.com>
2024-09-01 15:52:39 +0300

    Creating project

---
+++ src/main/java/org/example/calculatorsite/CalculatorSiteApplication.java
@@ -0,0 +1,13 @@
+package org.example.calculatorsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculatorSiteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CalculatorSiteApplication.class, args);
+    }
+
+}
